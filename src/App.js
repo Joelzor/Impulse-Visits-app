6a0e18e378e92e931c9398c6f7e0a13a0c61469f
Joel Watkins <eck087@yahoo.co.uk>
2022-12-07 15:17:22 +0000

    added routes, css reset and initial geolocation working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Activities from "./components/Activities";
 
-function App() {}
+const apiKey = "AIzaSyAFVPra5dqDvAbLHOh0wEw0u5bLCTocZro";
+
+function App() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/activities" element={<Activities />} />
+      </Routes>
+    </main>
+  );
+}
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  // const [userLatitude, setUserLatitude] = useState(null);
+  // const [userLongitude, setUserLongitude] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, []);
+
+  const successCallback = (position) => {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+  };
+
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+
   return <>Home Component</>;
 };
 
