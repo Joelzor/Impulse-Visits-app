c64b3864b5ef235143f17a46eb50ad21e55bdd62
Joel Watkins <eck087@yahoo.co.uk>
2022-12-08 09:50:23 +0000

    added some really basic CSS to homepage

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,4 +1,31 @@
+const apiKey = process.env.REACT_APP_API_KEY_OPEN_TRIP_MAP;
+
 const Activities = () => {
+  // const pageLength = 5; // number of objects per page
+  // let offset = 0; // offset from first object in the list
+  // let count; // total objects count
+  // apiGet(
+  //   "radius",
+  //   `radius=1000&limit=${pageLength}&offset=${offset}&lon=${userLongitude}&lat=${userLatitude}&rate=2&format=${count}`
+  // );
+
+  // function given to us by the Open Trip Map API (slightly adjusted)
+  // function apiGet(method, query) {
+  //   let otmAPI =
+  //     "https://api.opentripmap.com/0.1/en/places/" +
+  //     method +
+  //     "?apikey=" +
+  //     apiKey;
+  //   if (query !== undefined) {
+  //     otmAPI += "&" + query;
+  //   }
+  //   fetch(otmAPI)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data))
+  //     .catch(function (err) {
+  //       console.log("Fetch Error :-S", err);
+  //     });
+  // }
   return <>Activities Component</>;
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Home.css";
 
-const apiKey = process.env.REACT_APP_API_KEY_OPEN_TRIP_MAP;
 const apiKeyGeocode = process.env.REACT_APP_API_KEY_GEOCODE;
 
 const Home = () => {
@@ -35,13 +34,6 @@ const Home = () => {
         .catch((err) => {
           console.log(err);
         });
-      // const pageLength = 5; // number of objects per page
-      // let offset = 0; // offset from first object in the list
-      // let count; // total objects count
-      // apiGet(
-      //   "radius",
-      //   `radius=1000&limit=${pageLength}&offset=${offset}&lon=${userLongitude}&lat=${userLatitude}&rate=2&format=${count}`
-      // );
     }
   }, [userLatitude, userLongitude]);
 
@@ -55,33 +47,32 @@ const Home = () => {
     console.log(error);
   };
 
-  // function given to us by the Open Trip Map API (slightly adjusted)
-  // function apiGet(method, query) {
-  //   let otmAPI =
-  //     "https://api.opentripmap.com/0.1/en/places/" +
-  //     method +
-  //     "?apikey=" +
-  //     apiKey;
-  //   if (query !== undefined) {
-  //     otmAPI += "&" + query;
-  //   }
-  //   fetch(otmAPI)
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data))
-  //     .catch(function (err) {
-  //       console.log("Fetch Error :-S", err);
-  //     });
-  // }
-
   return (
     <>
-      <h1>Impulse Visits</h1>
-      <img
-        src="https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="city"
-        className="banner"
-      />
-      <p>Your location: {location ? location : "Unable to locate"}</p>
+      <div className="banner">
+        <h1 className="home-title">Impulse Visits</h1>
+        <img
+          src="https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="city"
+          className="banner-img"
+        />
+      </div>
+      <div className="location-container">
+        <p>
+          Your location:{" "}
+          <span className="location-text">
+            {location ? location : "Unable to locate"}
+          </span>
+        </p>
+        <button>Confirm</button>
+      </div>
+      <form className="search-form">
+        <input
+          type="search"
+          className="searchbar"
+          placeholder="Search for another city..."
+        />
+      </form>
     </>
   );
 };
