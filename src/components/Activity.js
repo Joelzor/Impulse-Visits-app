175efb7c1e73467b5a1f529229579358e4073be4
Joel Watkins <eck087@yahoo.co.uk>
2023-01-15 10:49:53 +0000

    re-added hover colour to activity component

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -23,7 +23,7 @@ const Activity = ({
   return (
     <div className="flex justify-between">
       <li
-        className={`mb-4 p-2 cursor-pointer rounded-lg w-[415px] ${highlightPlan}
+        className={`mb-4 p-2 cursor-pointer rounded-lg w-[415px] hover:bg-[#e1e0e0] ${highlightPlan}
           `}
         onClick={() => setCurrentPlan(activity)}
       >
